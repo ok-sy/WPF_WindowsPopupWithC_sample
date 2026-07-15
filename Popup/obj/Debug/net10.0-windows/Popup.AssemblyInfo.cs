f8506769babea2bdfd3cd095cbc80c3e8c329ef9
옥승엽 <oso792@naver.com>
2026-07-15 13:35:22 +0900

    button Hover

--- Popup/obj/Debug/net10.0-windows/Popup.AssemblyInfo.cs
+++ Popup/obj/Debug/net10.0-windows/Popup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Popup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61317b63570e93cfb930728955e89ef9a1e57cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b25c6b37eab206a29c733b213130cc5dc634807")]
 [assembly: System.Reflection.AssemblyProductAttribute("Popup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Popup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
